52711d2d4aa407d2b6d83596e9c2dc8c60c765f9
Kishore Ramakrishnan <rkishore0102@gmail.com>
2021-01-24 22:24:59 +0530

    env

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-const dotenv = require('dotenv');
-dotenv.config();
 
 console.log("node_env : " + process.env.NODE_ENV);
 console.log("api url : " + process.env.API_URL);
