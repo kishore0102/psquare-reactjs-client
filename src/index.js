2473d7720462154130babc356babf29c9226e6be
Kishore Ramakrishnan <rkishore0102@gmail.com>
2021-01-24 23:16:03 +0530

    env

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const dotenv = require('dotenv').config();
-console.log("dotenv - ", dotenv);
-console.log("NODE_ENV : " + process.env.NODE_ENV);
-console.log("API_URL : " + process.env.API_URL);
-console.log("REACT_APP_PSQUARE_API_URL : " + process.env.REACT_APP_PSQUARE_API_URL);
-
-axios.defaults.baseURL = process.env.PSQUARE_API_URL;
+axios.defaults.baseURL = process.env.REACT_APP_PSQUARE_API_URL;
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 ReactDOM.render(
