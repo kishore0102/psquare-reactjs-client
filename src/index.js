c3ca6065cbd0b0033e226c411bd41fe4b5b58254
Kishore Ramakrishnan <rkishore0102@gmail.com>
2021-01-24 21:55:13 +0530

    env

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+const dotenv = require('dotenv');
+dotenv.config();
 
-
-console.log("env api url - " + $API_URL);
+console.log("node_env : " + process.env.NODE_ENV);
+console.log("api url : " + process.env.API_URL);
 axios.defaults.baseURL = process.env.API_URL;
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
